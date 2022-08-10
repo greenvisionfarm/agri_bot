aec989fdcb087a50b15171383206a111b8118453
vladibuyanov <vladibuyanov@gmail.com>
2022-08-10 12:51:59 +0200

    Ver. 10/08/22

---
+++ configuration.py
@@ -0,0 +1,10 @@
+import os
+from sqlalchemy import create_engine
+from telebot import TeleBot
+
+# Telegram
+bot = TeleBot(os.environ.get('TOKEN'))
+# Data
+KEY = os.environ.get('KEY')
+URL = f'https://flux.agritel.com/agritelwebsite/QuotesAjax.aspx?key={KEY}'
+engine = create_engine('sqlite:///./agri_bot.db')
